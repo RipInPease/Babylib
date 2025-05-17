fb127ac565f92a4354d7d9bbcb645331ba437e17
RipInPease <svorrevfjelde@gmail.com>
2025-05-17 22:48:35 +0200

    vec2d

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
-pub mod rng {
-  pub fn rand (min: u32, max: u32) -> u32 {
-    use std::time::SystemTime;
-    let sys_time = SystemTime::now();
-    max
-  }
-}
+//pub mod rng {
+//  pub fn rand (min: u32, max: u32) -> u32 {
+//    use std::time::SystemTime;
+//    let sys_time = SystemTime::now();
+//    max
+//  }
+//}
 
 pub mod Vec2d {
-  pub fn new<T> (x: usize, y: usize) {
+  pub fn new<T> (x: usize, y: usize, value: T) -> Vec<Vec<T>>
+  where 
+    T: Clone {
+      vec![vec![value; y]; x]
+    }
 
-  }
 }
