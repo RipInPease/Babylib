986ee2ba01aca54538b6ad45ae0f14347435cf93
RipInPease <svorrevfjelde@gmail.com>
2025-10-31 14:14:53 +0100

    Trait and TON

--- src/timer/mod.rs
+++ src/timer/mod.rs
@@ -1,17 +1,52 @@
-use std::time::SystemTime;
+use std::time::Duration;
 
 /// Contains all different types of timers
 /// e.g. TON, TOF and much more (at some point)
 ///
-pub mod variants;
+mod variants;
+pub use variants::*;
 
 
-use variants::*;
+/// Defines how a timer works
+///
+pub trait Timer {
+
+    /// Get the status of the output
+    /// 
+    fn q(&self) -> bool;
 
 
+    /// Get the elapsed time.
+    /// Returns None if the timer has not been started
+    /// 
+    fn et(&self) -> Option<Duration> {
+        unimplemented!("et() not implemented for this timer")
+    }
 
 
-pub struct Timer {
-    variant: Variant,
-    start_time: SystemTime,
+    /// Gives the time until output of timer is goes high.
+    /// Returns None if timer is not enabled or the output is already high
+    /// 
+    fn time_remain(&self) -> Option<Duration> {
+        unimplemented!("time_remain() not implemented for this timer")
+    }
+
+
+    /// Set the start signal of the timer to true
+    /// 
+    fn start(&mut self) {
+        self.set_status(true);
+    }
+
+
+    /// Set the start signal of the timer to false, resets the elapsed time
+    /// 
+    fn stop(&mut self) {
+        self.set_status(false);
+    }
+
+
+    /// Sets the status of the input. On reset, elapsed time is reset
+    /// 
+    fn set_status(&mut self, status: bool);
 }
--- src/timer/variants/mod.rs
+++ src/timer/variants/mod.rs
@@ -1,26 +1,2 @@
-use std::time::Duration;
-
-pub enum Variant{
-    Ton{
-        t: Duration
-    },
-    Tof{
-        t: Duration
-    }
-}
-
-
-impl Variant{
-    /// Creates the TON variant with given set_time
-    /// 
-    pub fn ton(t: Duration) -> Self {
-        Self::Ton{t}
-    }
-
-    
-    /// Creates the TOF variant with given set_time
-    /// 
-    pub fn tof(t: Duration) -> Self {
-        Self::Tof{t}
-    }
-}
+mod ton;
+pub use ton::Ton;
